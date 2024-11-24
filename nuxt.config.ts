bb1bea3ebf883094d64aabca30a4c644a9110d67
OutlinedArc217 <z3407323905@outlook.com>
2024-11-24 12:27:41 +0800

    conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,63 +1,51 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { siteConfig } from './site.config'
-
 export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    strict: true,
+  },
   modules: [
     '@unocss/nuxt',
     '@vueuse/nuxt',
-    '@nuxt/content',
-    '@nuxtjs/stylelint-module',
   ],
   app: {
-    rootId: 'nuxt-root',
     head: {
       meta: [
-        { name: 'description', content: siteConfig.description },
-        { name: 'author', content: siteConfig.author },
         { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
         { name: 'revisit-after', content: '7 days' },
         { name: 'msapplication-TileColor', content: '#ffffff' },
         { charset: 'UTF-8' },
         { 'http-equiv': 'X-UA-Compatible', 'content': 'IE=edge' },
       ],
-      noscript: [
-        { children: 'JavaScript is required' },
+      script: [
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-MEC21CTMC9',
+          async: '',
+        },
+        {
+          innerHTML: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MEC21CTMC9');
+          `,
+        },
       ],
-      htmlAttrs: {
-        lang: siteConfig.lang,
-      },
+      noscript: [{ children: 'JavaScript is required' }],
       bodyAttrs: {
         class: 'font-sans',
       },
     },
   },
-  content: {
-    highlight: {
-      theme: {
-        // Default theme (same as single string)
-        default: 'vitesse-light',
-        // Theme used if `html.dark`
-        dark: 'vitesse-dark',
-        // Theme used if `html.sepia`
-        sepia: 'monokai',
-      },
-      preload: [
-        'c',
-        'cpp',
-        'java',
-      ],
-
-    },
-  },
   css: [
-    '@unocss/reset/tailwind.css',
-    '@/assets/styles/global.scss',
-    '@/assets/styles/theme.css',
+    '@unocss/reset/tailwind-compat.css',
+    '@/assets/styles/global.css',
+    '@/assets/styles/var.css',
     '@/assets/styles/transition.css',
     '@/assets/styles/markdown.scss',
   ],
-  stylelint: {
-    /* module options */
-    lintOnStart: false,
+  devServer: {
+    port: 4000,
   },
 })
