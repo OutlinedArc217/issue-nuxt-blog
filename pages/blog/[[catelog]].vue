e09bbb189aedad25cc91287c191d8dca16767c29
OutlinedArc217 <z3407323905@outlook.com>
2024-11-24 15:39:11 +0800

    u

--- pages/blog/[[catelog]].vue
+++ pages/blog/[[catelog]].vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { searchIssues } from '@/api'
-import { watch, ref } from 'vue'
-import { useRoute, useHead } from 'vue-router'
+import { ref, watch } from 'vue'
+import { useRoute, useHead } from '#imports' // 使用 Nuxt 3 提供的全局方法
 
+// 获取当前路由参数
 const route = useRoute()
+
 // 设置初始分类，默认值为 'All'
 const catelog = ref(route.params.catelog || 'All')
 
+// 动态设置页面标题
 useHead({
   title: catelog.value,
 })
@@ -16,9 +18,11 @@ const issueList = ref()
 
 // 搜索函数
 async function handleSearch(q: string) {
-  const { data } = catelog.value !== 'All' 
-    ? await searchIssues(q, { milestone: catelog.value }) 
-    : await searchIssues(q)
+  // 如果分类是 'All'，不传 milestone 参数
+  const { data } = catelog.value !== 'All'
+    ? await $fetch('/api/searchIssues', { params: { q, milestone: catelog.value } })
+    : await $fetch('/api/searchIssues', { params: { q } })
+
   searchResult.value = data.value
   issueList.value = insertYearToPosts(data.value?.items)
 }
@@ -31,8 +35,9 @@ watch(
   () => route.params.catelog,
   (newCatelog) => {
     catelog.value = newCatelog || 'All'
+    useHead({ title: catelog.value }) // 更新页面标题
     handleSearch('') // 重新触发搜索
-  },
+  }
 )
 </script>
 
